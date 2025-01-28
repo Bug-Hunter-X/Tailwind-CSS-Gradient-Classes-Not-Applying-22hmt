a36d45601716a140d67f92b10d3f49a7255a1f9d
BugHunter-X <askzenai@gmail.com>
2025-01-28 23:52:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Some Tailwind classes aren't applied because of a missing plugin or improper configuration.
+// Example:
+/* This will cause an error because the plugin is not configured correctly. */
+<div class="bg-gradient-to-r from-blue-500 to-purple-500">Some text</div>
+```
